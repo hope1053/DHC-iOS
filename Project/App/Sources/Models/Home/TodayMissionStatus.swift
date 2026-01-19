03d7213f14ed06bc5fe26f362d2d626d7b6d70b1
hyerin <hope1053@megastudy.net>
2026-01-19 11:30:01 +0900

    feat: 오늘 미션 완료처리 후 미션 상태 DTO, Entity 수정

---
+++ Project/App/Sources/Models/Home/TodayMissionStatus.swift
@@ -0,0 +1,13 @@
+//
+//  TodayMissionStatus.swift
+//  Flifin
+//
+//  Created by hyerin on 1/19/26.
+//
+
+import Foundation
+
+struct TodayMissionStatus {
+  let isMissionSuccess: Bool
+  let earnedPoint: Int
+}
