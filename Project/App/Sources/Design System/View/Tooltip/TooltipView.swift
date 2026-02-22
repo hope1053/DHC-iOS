2290b49956ee66bacd1b6becb72430a999a0317f
hyerin <hope1053@naver.com>
2026-02-22 18:41:20 +0900

    feat: Tooltip 구현 수정

--- Project/App/Sources/Design System/View/Tooltip/TooltipView.swift
+++ Project/App/Sources/Design System/View/Tooltip/TooltipView.swift
@@ -17,7 +17,7 @@ enum Tooltip {
     case .onboardingGradient:
       return AnyShapeStyle(LinearGradient(.tooltip01))
     case .rewardGradient:
-      return AnyShapeStyle(LinearGradient(.fortuneBorderLow).opacity(0.28))
+      return AnyShapeStyle(Color.clear)
     case .solid:
       return AnyShapeStyle(ColorResource.Neutral._500.color)
     }
@@ -25,8 +25,10 @@ enum Tooltip {
   
   var foregroundColor: Color {
     switch self {
-    case .onboardingGradient, .solid:
+    case .onboardingGradient:
       ColorResource.Background.main.color
+    case .solid:
+      ColorResource.Text.main.color
     case .rewardGradient:
       ColorResource.Text.Highlights.primary.color
     }
@@ -43,14 +45,7 @@ enum Tooltip {
         )
       )
     case .rewardGradient:
-      return AnyShapeStyle(
-        LinearGradient(
-          .fortuneBorderLow,
-          startPoint: .bottom,
-          endPoint: .top
-        )
-        .opacity(0.28)
-      )
+      return AnyShapeStyle(Color.clear)
     case .solid:
       return AnyShapeStyle(ColorResource.Neutral._500.color)
     }
@@ -84,9 +79,23 @@ struct TooltipView: View {
   }
   
   var body: some View {
-    VStack(spacing: 0) {
-      contentView
-      bottomArrowView
+    switch type {
+    case .rewardGradient:
+      ZStack(alignment: .bottom) {
+        ChromeMaterialBlurView(style: .systemUltraThinMaterial)
+          .mask(
+            TooltipShape(cornerRadius: 8, arrowWidth: 12, arrowHeight: 6)
+          )
+        textView
+          .padding(10)
+          .padding(.bottom, 6) // arrow 높이만큼 여백
+      }
+    default:
+      VStack(spacing: 0) {
+        contentView
+        bottomArrowView
+          .frame(width: 12, height: 6)
+      }
     }
   }
   
@@ -118,17 +127,15 @@ struct TooltipView: View {
       .multilineTextAlignment(.center)
   }
   
+  @ViewBuilder
   private var bottomArrowView: some View {
-    Group {
-      switch type {
-      case .rewardGradient:
-        ChromeMaterialBlurView(style: .systemUltraThinMaterial)
-          .mask(BottomRoundedInvertedTriangle(cornerRadius: 1))
-      default:
-        BottomRoundedInvertedTriangle(cornerRadius: 1)
-          .fill(type.bottomArrowBackgroundColor)
-      }
+    switch type {
+    case .rewardGradient:
+      ChromeMaterialBlurView(style: .systemUltraThinMaterial)
+        .mask(BottomRoundedInvertedTriangle(cornerRadius: 1))
+    default:
+      BottomRoundedInvertedTriangle(cornerRadius: 1)
+        .fill(type.bottomArrowBackgroundColor)
     }
-    .frame(width: 12, height: 6)
   }
 }
